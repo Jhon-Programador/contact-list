51949612d2f0a8bb0e0fb0bbb6562239aa2a5842
Jhon-Programador <ProgramadorJunior>
2023-12-04 14:52:16 -0500

    He actualizado la funciones para crear y eliminar contactos

---
+++ java.js
@@ -0,0 +1,35 @@
+define([
+    'require',
+    'dependency'
+], function(require, factory) {
+    'use strict';
+    
+}); añadir_contacto(contactos, contacto)    contactos.append(contacto)
+
+#Ejemplodeuso
+añadir_contacto(contactos, "Sofía Gómez")
+
+//Resultado
+contactos = [
+    "Juan Pérez",
+    "María García",
+    "Pedro López",
+    "Ana Rodríguez",
+    "José Fernández",
+    "Sofía Gómez",
+]
+//def borrar_contacto
+(contactos, contacto):
+    contactos.remove(contacto)
+
+//Ejemplo de uso
+borrar_contacto(contactos, "Juan Pérez")
+
+//Resultado
+contactos = [
+    "María García",
+    "Pedro López",
+    "Ana Rodríguez",
+    "José Fernández",
+    "Sofía Gómez",
+]
